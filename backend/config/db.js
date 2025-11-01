5b7adefa49c806274cf6d10d0679d55ea5fe55f7
Rishank <rishankrawat043@gmail.com>
2025-11-01 12:43:55 +0530

    old

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,50 +1,15 @@
-// const mongoose = require('mongoose');
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI, {
-//         }).then(() => {
-//             console.log('MongoDB connected successfully');
-//         });
-        
-//     } catch (error) {
-//         console.error('MongoDB connection error:', error);
-//         process.exit(1);
-//     }
-// };
-
-// module.exports = connectDB;
-
-
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("❌ MongoDB connection error:", error.message);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+        }).then(() => {
+            console.log('MongoDB connected successfully');
+        });
+        
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
-
-
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI);
-
-//     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error("❌ MongoDB connection error:", error.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
